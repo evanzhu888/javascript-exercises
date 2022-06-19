a0061a8e1c8d3fcb2d85992683218eb73cab0050
Evan Zhu <evanzhu888@gmail.com>
2022-06-19 00:54:59 -0400

    Code 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
 
+    for (let i = 0; i < arr.length; i++) {
+        for (const a of args) {
+            if (a == arr[i] && typeof arr[i] == typeof a) {
+                arr.splice(i, 1);
+                i--;
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
