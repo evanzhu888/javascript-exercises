33bb357fb77d7359e3c6bfa3aac9a950529f591b
Evan Zhu <evanzhu888@gmail.com>
2022-06-21 18:13:36 -0400

    Code 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,18 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const string = Array.from(str).filter(char => (char != ' ' 
+    && char != ',' && char != '.' && char != '!')).join("").toLowerCase();
+
+    let i = 0;
+    let j = string.length - 1;
+
+    while (i < j) {
+        if (string.charAt(i) != string.charAt(j)) {
+            return false;
+        }
+        i++;
+        j--;
+    }
+    return true;
 
 };
 
