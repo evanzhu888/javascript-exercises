9c0f01215c3cad6331e36194ca7fc0909995eed6
Evan Zhu <evanzhu888@gmail.com>
2022-06-21 18:21:33 -0400

    Code 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,11 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 0) {
+        return "OOPS";
+    }
+    if (num == 1 || num == 2) {
+        return 1;
+    }
+    return fibonacci(num - 2) + fibonacci(num - 1);
 
 };
 
