19008e58eddb5db94b837e2898d793c455c9ed95
Evan Zhu <evanzhu888@gmail.com>
2022-06-21 18:41:45 -0400

    Code 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    let j = 0;
+    let maxAge = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (!arr[i].yearOfDeath) {
+            arr[i].yearOfDeath = 2022;
+        }
+        if (arr[i].yearOfDeath - arr[i].yearOfBirth > maxAge) {
+            j = i;
+            maxAge = arr[i].yearOfDeath - arr[i].yearOfBirth;
+        }
+    }
+
+    return arr[j];
 
 };
 
